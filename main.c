8414fe6b46ba05f5172eb5dc3c0a2ea2a3283ec7
Abdullah Asghar <dulakhan024@gmail.com>
2023-05-15 10:37:12 +0200

    Delete extra code

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
+#define F_CPU 16000000UL
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "Stepper.h"
 
-
-// Define color sensor pins
 #define S0 4
 #define S1 5
 #define S2 6
@@ -19,38 +18,32 @@ const int stepPin2 = 4; const int dirPin2 = 5;
 const int stepPin3 = 6; const int dirPin3 = 7;
 const int stepPin4 = 8; const int dirPin4 = 9;
 
-//Inicialize the steppermotors
-Stepper motor1(stepsPerRevolution, stepPin1, dirPin1);
-Stepper motor2(stepsPerRevolution, stepPin2, dirPin2);
-Stepper motor3(stepsPerRevolution, stepPin3, dirPin3);
-Stepper motor4(stepsPerRevolution, stepPin4, dirPin4);
-
-// Calibration Values
-// Get these from Calibration Sketch
+//Variables for color sensor
+/*Variable from calibration*/
 int redMin = 19; // Red minimum value
 int redMax = 194; // Red maximum value
 int greenMin = 18; // Green minimum value
 int greenMax = 235; // Green maximum value
 int blueMin = 23; // Blue minimum value
 int blueMax = 187; // Blue maximum value
-
-// Variables for Color Pulse Width Measurements
+/*Variables for Color Pulse Width*/
 int redPW = 0;
 int greenPW = 0;
 int bluePW = 0;
-
-// Variables for final Color values
+/*Final color value*/
 int redValue;
 int greenValue;
 int blueValue;
 
-// Function prototypes
-int getRedPW();
-int getGreenPW();
-int getBluePW();    
+//Inicialize the steppermotors
+Stepper motor1(stepsPerRevolution, stepPin1, dirPin1);
+Stepper motor2(stepsPerRevolution, stepPin2, dirPin2);
+Stepper motor3(stepsPerRevolution, stepPin3, dirPin3);
+Stepper motor4(stepsPerRevolution, stepPin4, dirPin4);
 
+//Initialize the color sensor
+void setup() {
 
-    int main (void){
 
     // Set S0 - S3 as outputs
     pinMode(S0, OUTPUT);
@@ -65,22 +58,36 @@ int getBluePW();
     digitalWrite(S0, HIGH);
     digitalWrite(S1, LOW);
 
-    // Calibration loop, still left to add Libraries and LCD_code to show it in the monitor
-    printf("Calibrating...\n");
-    for (int i = 0; i < 10; i++) {
-        // Read Red value
-        redPW = getRedPW();
-        if (redPW < redMin) {
-            redMin = redPW;
-        }
-        if (redPW > redMax) {
-            redMax = redPW;
-        }
-        delay(100);
+    // Setup Serial Monitor
+    Serial.begin(9600);}
 
-return 0;
+// Declare color sensor functions
+void loop() {
+
+    // Read Red value
+    redPW = getRedPW();
+    // Map to value from 0-255
+    redValue = map(redPW, redMin, redMax, 255, 0);
+    // Delay to stabilize sensor
+    delay(200);
+
+    // Read Green value
+    greenPW = getGreenPW();
+    // Map to value from 0-255
+    greenValue = map(greenPW, greenMin, greenMax, 255, 0);
+    // Delay to stabilize sensor
+    delay(200);
+
+    // Read Blue value
+    bluePW = getBluePW();
+    // Map to value from 0-255
+    blueValue = map(bluePW, blueMin, blueMax, 255, 0);
+    // Delay to stabilize sensor
+    delay(200);
+
+    // Print output to Serial Monitor
+    printf("Red = %d - Green = %d - Blue = %d\n", redValue, greenValue, blueValue);
 }
-    }
 
 // Function for moving the motors
 void moverCilindros(int distancia) {
@@ -94,3 +101,8 @@ motor2.setSpeed(100);motor2.step(-pasos);
 motor3.setSpeed(100);motor3.step(pasos);
 motor4.setSpeed(100);motor4.step(-pasos);
 }
+
+int main (void){
+
+}
+
